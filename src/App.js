5a48edc5dc140ef8e20b958255fd34d135cc1547
lax1n <august.haug@gmail.com>
2017-11-01 22:02:16 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Stock from './screens/stock';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Stock />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/screens/stock.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+class Stock extends Component {
+  render() {
+    return (
+      <div className="Stock">
+      </div>
+    );
+  }
+}
+
+export default Stock;
